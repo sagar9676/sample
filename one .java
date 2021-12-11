12fae479510bf1c8a19c518eacef0685f24f6d2a
Kancharla Sagar <sagarchowdary0005@gmail.com>
2021-12-11 11:54:25 +0530

    first commit

---
+++ one .java
@@ -0,0 +1,12 @@
+static void numberToWords(char num[])  
+{  
+//determines the number of digits in the given number  
+int len = num.length;  
+//checks the given number has number or not  
+if (len == 0)   
+{  
+//if the given number is empty prints the following statement     
+System.out.println("The string is empty.");  
+return;  
+}  
+
